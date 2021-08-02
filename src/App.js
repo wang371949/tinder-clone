83776ff60f50690a91e4f8f0c3a136a36f02facd
Jiawei Wang <wang371949@gmail.com>
2021-08-02 21:52:48 +1000

    add swipe buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="app">
     <Header />
     <TinderCards />
-    {/* SwipeButtons */}
+    <SwipeButtons />
     </div>
   );
 }
